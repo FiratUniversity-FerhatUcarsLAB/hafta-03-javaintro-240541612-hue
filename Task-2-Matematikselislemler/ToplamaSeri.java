81b8024dca93463b231cd13af973a0bc51c2519f
240541612-hue <240541612@firat.edu.tr>
2025-10-23 22:26:04 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,10 @@
+
+    public class ToplamaSerijava {
+    public static void main(String[] args) {
+     int x=1+2+3+4+5+6+7+8+9;
+     // x'i hesapla (bu bir yorum)
+     System.out.println("1+2+3+4+5+6+7+8+9=" +x);
+     //yazdır
+    }
+
+}
