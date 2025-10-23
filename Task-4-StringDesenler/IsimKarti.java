606868626149bff84f6870906df63007c089cc9c
240541612-hue <240541612@firat.edu.tr>
2025-10-23 22:34:45 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,18 @@
+
+     public class İsimKarti {
+    public static void main(String[] args) {
+    String ad= "Amr";
+    String soyad="Mohammed";
+    String ogrno="240541612";
+    String bolum="yazilim Muh.";
+
+    System.out.println(",,,,,,,,,,,,,");
+    System.out.println("Ogrenci bilgileri");
+    System.out.println(",,,,,,,,,,,,,,,,,,,,");
+    System.out.println("Ad :" + ad);
+    System.out.println("Soyad  :"  +soyad);
+    System.out.println("No :" +ogrno);
+    System.out.println("BOlum :"+bolum);
+     System.out.println(",,,,,,,,,,");
+    }
+}
