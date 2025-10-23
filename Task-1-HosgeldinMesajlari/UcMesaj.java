a91c5cd5360d141e760bece00974db4b74150730
240541612-hue <240541612@firat.edu.tr>
2025-10-23 22:22:39 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,12 @@
+
+
+public class UcMesaj {
+    public static void main(String[] args)  {
+        System.out.println("Javaya Hoş Geldiniz");
+        System.out.println("Bilgisayar Bilimine HoşGeldiniz")
+        System.out.println("Proglamlama Eğlencelidir");
+        
+    }
+
+}
+
